6c28b9ccdf2c2d65ed62f8083f1e72900131d411
Chiara Seco <chiaraseco7@gmail.com>
2023-04-27 18:18:09 -0300

    #

--- division.py
+++
@@ -1,7 +0,0 @@
-def division(a, b):
-    if(b != 0):
-        print('El resultado de la división es: ', a / b)
-    else:
-        print('Usted ha ingresado un divisor con valor ', b, ', por lo que no es posible realizar la división.')
-
-division(10, 5)
